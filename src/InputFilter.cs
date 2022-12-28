32e59ae3967a92138901ba4bc8ad3c0680c317ae
Josepe <yosefd99@gmail.com>
2022-12-28 14:30:10 +0200

    Update InputFilter.cs

--- src/InputFilter.cs
+++ src/InputFilter.cs
@@ -1,106 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace KeyBinder
 {
-    /// Source code & Documentation: https://github.com/JosepeDev/KeyBinder
+    /// Source code & Documentation: https://github.com/jozzzzep/KeyBinder
     /// <summary>
     /// The class that filters the input a <see cref="KeyDetector"/> receives
     /// </summary>
     public class InputFilter
     {
-        /// <summary>
-        /// The key's that are valid for the <see cref="KeyDetector"/> to receive, if empty - all keys are valid
-        /// </summary>
-        public KeyCode[] ValidKeys { get; private set; }
+        /// Properties ------------
+        /// - Keys                - The key's that are valid/invalid for the KeyDetector to receive
+        /// - FilteringActive     - Determines whether the filter is filtering
+        /// - FilteringMethod     - The filtering method the input filter uses
+        /// -----------------------
 
         /// <summary>
-        /// Determines whether the key detector input filtering is active, 
-        /// clear all valid keys to deactivate, add valid keys to activate filtering
+        /// Determines the filtering method the <see cref="InputFilter"/> uses 
+        /// </summary>
+        public enum Method
+        {
+            /// <summary>
+            /// The key will be valid only if it is inside the keys list
+            /// </summary>
+            Whitelist,
+            /// <summary>
+            /// If the key is inside the keys list it'll be invalid
+            /// </summary>
+            Blacklist
+        }
+
+        /// <summary>
+        /// The key's that are valid/invalid for the <see cref="KeyDetector"/> to receive, if empty - all keys are valid
+        /// </summary>
+        public KeyCode[] Keys => keysList;
+
+        /// <summary>
+        /// Determines whether the key detector input filtering is active
         /// </summary>
         public bool FilteringActive { get; private set; }
 
         /// <summary>
-        /// Raised when an invalid key has been detected
+        /// The filtering method the input filter uses
         /// </summary>
-        public event Action<KeyCode> InvalidKeyReceived;
-
-        internal InputFilter()
-        {
-            ValidKeys = new KeyCode[0];
-            FilteringActive = false;
-        }
+        public Method FilteringMethod { get; private set; }
+        
+        private KeyCode[] keysList;
+        private HashSet<KeyCode> keysHashSet;
 
         /// <summary>
-        /// Set the valid keys array
+        /// Creates an inactive empty filter
         /// </summary>
-        public void ValidKeysSet(KeyCode[] keys)
-        {
-            ValidKeys = keys;
-            DetermineFilterActivity();
-        }
+        internal InputFilter() : 
+            this(new List<KeyCode>().ToArray(), Method.Whitelist)
+        { }
 
         /// <summary>
-        /// If you want to add a key for the input filering list
+        /// Creates a filter from a preset
         /// </summary>
-        /// <param name="key">The key you want to add</param>
-        public void ValidKeysAdd(KeyCode key)
-        {
-            var current = new List<KeyCode>(ValidKeys);
-            current.Add(key);
-            ValidKeys = current.ToArray();
-            DetermineFilterActivity();
-        }
+        internal InputFilter(IInputFilterPreset preset) :
+            this(preset.KeysList, preset.FilteringMethod)
+        { }
 
         /// <summary>
-        /// If you want to add multiple keys for the input filering list
+        /// Creates a filter, if the array of keys contains at least one key, it will activate the filter
         /// </summary>
-        /// <param name="keys">Array with the keys you want to add</param>
-        public void ValidKeysAdd(IEnumerable<KeyCode> keys)
+        /// <param name="keys">List of keys to filter</param>
+        /// <param name="filteringMethod"></param>
+        internal InputFilter(KeyCode[] keys, Method filteringMethod = Method.Whitelist)
         {
-            var current = new List<KeyCode>(ValidKeys);
-            current.AddRange(keys);
-            ValidKeys = current.ToArray();
-            DetermineFilterActivity();
-        }
-
-        /// <summary>
-        /// Removes a certain KeyCode from the list of valid keys if it exists there
-        /// </summary>
-        /// <param name="key">A key you want to remove from the list of valid keys.</param>
-        public void ValidKeysRemove(KeyCode key)
-        {
-            var current = new List<KeyCode>(ValidKeys);
-            if (current.Count > 0)
-                if (current.Contains(key))
-                    current.Remove(key);
-            ValidKeys = current.ToArray();
-            DetermineFilterActivity();
-        }
-
-        /// <summary>
-        /// Removes a bunch of KeyCodes from the valid keys if they are contained 
-        /// </summary>
-        /// <param name="key">An array of keys you want to remove from the list of valid keys.</param>
-        public void ValidKeysRemove(KeyCode[] keys)
-        {
-            var current = new List<KeyCode>(ValidKeys);
-            if (current.Count > 0)
-                for (int i = 0; i < keys.Length; i++)
-                    if (current.Contains(keys[i]))
-                        current.Remove(keys[i]);
-            ValidKeys = current.ToArray();
-            DetermineFilterActivity();
-        }
-
-        /// <summary>
-        /// Clears the list of valid keys and turns of input filtering
-        /// </summary>
-        public void ValidKeysRemoveAll()
-        {
-            ValidKeys = null;
-            DetermineFilterActivity();
+            FilteringMethod = filteringMethod;
+            keysList = keys;
+            if (keysList != null)
+            {
+                keysHashSet = new HashSet<KeyCode>(keysList.Distinct());
+                FilteringActive = keysHashSet != null && keysHashSet.Count > 0;
+            }
+            else
+                FilteringActive = false;
         }
 
         /// <summary>
@@ -108,23 +86,17 @@ namespace KeyBinder
         /// </summary>
         /// <param name="key">The key</param>
         /// <returns>true if the key is valid</returns>
-        public bool IsKeyValid(KeyCode key)
+        internal bool IsKeyValid(KeyCode key)
         {
+            bool valid = true;
             if (FilteringActive)
             {
-                for (int i = 0; i < ValidKeys.Length; i++)
-                    if (ValidKeys[i] == key)
-                        return true;
-                InvalidKeyReceived.SafeInvoke(key);
-                return false;
+                valid = 
+                    FilteringMethod == Method.Whitelist ?
+                    keysHashSet.Contains(key) :
+                    !keysHashSet.Contains(key);
             }
-            return true;
+            return valid;
         }
-
-        /// <summary>
-        /// Called everytime the valid keys array changes
-        /// </summary>
-        private void DetermineFilterActivity() =>
-            FilteringActive = ValidKeys != null && ValidKeys.Length > 0;
     }
 }
