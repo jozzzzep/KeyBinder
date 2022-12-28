a540b592f1813f0a99c14aa219b979181c384b22
Josepe <yosefd99@gmail.com>
2022-12-28 14:30:15 +0200

    Update KeyDetector.cs

--- src/KeyDetector.cs
+++ src/KeyDetector.cs
@@ -3,12 +3,33 @@ using UnityEngine;
 
 namespace KeyBinder
 {
-    /// Source code & Documentation: https://github.com/JosepeDev/KeyBinder
+    /// Source code & Documentation: https://github.com/jozzzzep/KeyBinder
     /// <summary>
     /// A class for detecting which keys are being press
     /// </summary>
     public class KeyDetector : MonoBehaviour
     {
+        /// Properties ---------------------------
+        /// - InputCheckIsActive                 - Determines if the KeyDetector is currently checking for input
+        /// - InputFilter                        - The input filter of the detector
+        /// - LatestKey                          - Returns the latest key the KeyDetector received
+        /// --------------------------------------
+        /// 
+        /// Events -------------------------------
+        /// - KeyReceived(KeyCode)               - Raised when a valid key has been received by the key detector
+        /// - InvalidKeyReceived(KeyCode)        - Raised when an invalid key has been detected
+        /// --------------------------------------
+        /// 
+        /// Methods ------------------------------
+        /// - InputCheckOnce(Action<KeyCode>)    - Waits until a key is detected, calls the action, and turns off input checking
+        /// - InputCheckSetActive(bool)          - Set whether you want to check for input, keeps checking until deactivation
+        /// - RemoveAllListeners()               - Removes all the listeners from the KeyReceived event
+        /// ---                                ---
+        /// - SetInputFilter(InputFilter)        - Used to set custom filtering for the KeyDetector
+        /// - SetInputFilter(IInputFilterPreset) - Used to set custom filtering for the KeyDetector
+        /// - DisableInputFilter()               - Disables input filtering
+        /// --------------------------------------
+
         private static Action<KeyCode> singleDetectionAction;
         private static KeyDetector _instance;
         private static KeyDetector Instance
@@ -16,7 +37,11 @@ namespace KeyBinder
             get
             {
                 if (_instance == null)
-                    _instance = Initialize();
+                {
+                    _instance = Extensions.Initialize<KeyDetector>("KeyDetector");
+                    if (!_instance.isInitialized)
+                        KeyReceived = null;
+                }
                 return _instance;
             }
         }
@@ -24,10 +49,11 @@ namespace KeyBinder
         /// <summary>
         /// Determines if the <see cref="KeyDetector"/> is currently checking for input.
         /// </summary>
-        public static bool InputCheckIsActive => Instance.isActive;
+        public static bool InputCheckIsActive => Instance.keyCheckIsActive;
 
         /// <summary>
-        /// The input filter of the detector, inactive by default, add valid keys to activate filtering
+        /// The input filter of the detector, can be set by:
+        /// <see cref="SetInputFilter(IInputFilterPreset)"/> and  <see cref="SetInputFilter(InputFilter)"/>
         /// </summary>
         public static InputFilter InputFilter => Instance.inputFilter;
 
@@ -37,35 +63,22 @@ namespace KeyBinder
         public static KeyCode LatestKey => Instance.latestKey;
 
         /// <summary>
-        /// Called when a key has been received by key detector
+        /// Raised when a valid key has been received by the key detector
         /// </summary>
         public static event Action<KeyCode> KeyReceived;
-        
-        private InputFilter inputFilter;
-        private KeyCode latestKey = KeyCode.None;
-        private bool isActive = false;
 
         /// <summary>
-        /// Initialize the keydetector and make sure there's only a single instance of it
+        /// Raised when an invalid key has been detected
         /// </summary>
-        private static KeyDetector Initialize()
-        {
-            var keyDetectors = FindObjectsOfType<KeyDetector>();
-            if (keyDetectors == null || keyDetectors.Length == 0)
-            {
-                var obj = new GameObject("KeyBinder");
-                var keyDetector = obj.AddComponent<KeyDetector>();
-                keyDetector.inputFilter = new InputFilter();
-                return keyDetector;
-            }
-            else if (keyDetectors.Length > 1)
-                for (int i = 1; i < keyDetectors.Length; i++)
-                    Destroy(keyDetectors[i].gameObject);
-            return keyDetectors[0];
-        }
+        public static event Action<KeyCode> InvalidKeyReceived;
+
+        private InputFilter inputFilter = new InputFilter();
+        private KeyCode latestKey = KeyCode.None;
+        private bool keyCheckIsActive = false;
+        private bool isInitialized = false;
 
         /// <summary>
-        /// Waits until a key is pressed, calls the action, and turns off the input checking
+        /// Waits until a key is detected, calls the action, and turns off input checking
         /// </summary>
         public static void InputCheckOnce(Action<KeyCode> action)
         {
@@ -75,10 +88,10 @@ namespace KeyBinder
         }
 
         /// <summary>
-        /// Set whether you want to check for input
+        /// Set whether you want to check for input, keeps checking until deactivation
         /// </summary>
         public static void InputCheckSetActive(bool active) =>
-            Instance.isActive = active;
+            Instance.keyCheckIsActive = active;
 
         /// <summary>
         /// Removes all the listeners from the <see cref="KeyReceived"/> event
@@ -86,13 +99,27 @@ namespace KeyBinder
         public static void RemoveAllListeners() =>
             KeyReceived = null;
 
+        /// <summary>
+        /// Used to set custom filtering for the <see cref="KeyDetector"/>
+        /// </summary>
+        /// <param name="filter">An <see cref="KeyBinder.InputFilter"/> object</param>
+        public static void SetInputFilter(InputFilter filter) => Instance.inputFilter = filter ?? new InputFilter();
+
+        /// <summary>
+        /// Used to set custom filtering for the <see cref="KeyDetector"/>
+        /// </summary>
+        /// <param name="filterPreset">An <see cref="KeyBinder.IInputFilterPreset"/> object</param>
+        public static void SetInputFilter(IInputFilterPreset filterPreset) =>
+            Instance.inputFilter = (filterPreset != null) ? new InputFilter(filterPreset) : new InputFilter();
+
+        /// <summary>
+        /// Disables input filtering
+        /// </summary>
+        public static void DisableInputFilter() => SetInputFilter(new InputFilter());
 
         private static void OnKeyReceived(KeyCode key)
         {
-            var e = KeyReceived;
-            if (e != null)
-                e(key);
-
+            KeyReceived.SafeInvoke(key);
             if (singleDetectionAction != null)
             {
                 singleDetectionAction(key);
@@ -101,12 +128,16 @@ namespace KeyBinder
             }
         }
 
+        private static void OnInvalidKeyReceived(KeyCode key) =>
+            InvalidKeyReceived.SafeInvoke(key);
+
         private void Update()
         {
             // if the input checking is active
             // it checks for input
             // when a key is pressed it calls ReceiveInput().
-            if (isActive)
+
+            if (keyCheckIsActive)
                 if (Input.anyKey)
                     ReceiveInput();
         }
@@ -121,18 +152,19 @@ namespace KeyBinder
             return KeyCode.None;
         }
 
-        // Returns the pressed key
         private void ReceiveInput()
         {
-            KeyCode thePressedKey = GetPressedKey();
-            if (thePressedKey != KeyCode.None)
+            var key = GetPressedKey();
+            if (key != KeyCode.None)
             {
-                if (inputFilter.IsKeyValid(thePressedKey))
+                if (!inputFilter.IsKeyValid(key))
                 {
-                    latestKey = thePressedKey;
-                    OnKeyReceived(thePressedKey);
+                    OnInvalidKeyReceived(key);
+                    return;
                 }
+                latestKey = key;
+                OnKeyReceived(key);
             }
-        }
+        }    
     }
 }
